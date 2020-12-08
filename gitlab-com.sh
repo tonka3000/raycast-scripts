302e764991846cc07a01eefea1b49360c8f36b8f
tonka3000 <tonka3100@gmail.com>
2020-12-08 23:07:46 +0100

    add gitlab.com

---
+++ gitlab-com.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# @raycast.title GitLab.com
+# @raycast.author Michael Aigner
+# @raycast.authorURL https://github.com/tonka3000
+# @raycast.description Open gitlab.com in the default browser
+
+# @raycast.icon images/gitlab.png
+# @raycast.mode silent
+# @raycast.packageName GitLab
+# @raycast.schemaVersion 1
+
+open https://gitlab.com/
